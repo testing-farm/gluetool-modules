5f5b1941ae48e9c62bc4b90ff9f969295e344108
Miroslav Vadkerti <mvadkert@redhat.com>
2021-11-16 13:38:44 +0100

    test_schedule_tmt: fail reasonably if no plans found
    Currently the assert fails, user has no idea ;(
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- gluetool_modules/testing/test_schedule_tmt.py
+++ gluetool_modules/testing/test_schedule_tmt.py
@@ -336,7 +336,8 @@ class TestScheduleTMT(Module):
             )
             raise GlueError('Failed to list plans, TMT metadata are absent or corrupted.')
 
-        assert tmt_output.stdout
+        if not tmt_output.stdout:
+            raise GlueError("Did not find any plans. Command used '{}'.".format(' '.join(command)))
 
         output_lines = [line.strip() for line in tmt_output.stdout.splitlines()]
 

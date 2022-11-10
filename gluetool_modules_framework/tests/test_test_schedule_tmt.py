1dbcbd7b43ef0cbd54209671359e498842b4a6b2
Martin Pitt <mpitt@redhat.com>
2022-11-10 12:31:39 +0100

    test_schedule_tmt: Add unit test for results XML generation
    In particular, check that it picks up tmt's logs as expected. This
    currently only looks at the first log of tmt's results.yaml. That will
    be extended later, but capture the status quo for now.
    
    This has to add some necessary setup and mocks for `tmt run` and guest.
    These are already present in the `testing-farm` branch for other unit
    tests, and thus will be added eventually anyway.

--- gluetool_modules_framework/tests/test_test_schedule_tmt.py
+++ gluetool_modules_framework/tests/test_test_schedule_tmt.py
@@ -2,12 +2,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import os
+import shutil
+from mock import MagicMock
 
 import pytest
 
 import gluetool
 from gluetool_modules_framework.libs.testing_environment import TestingEnvironment
 import gluetool_modules_framework.testing.test_schedule_tmt
+from gluetool_modules_framework.infrastructure.distgit import DistGit, DistGitRepository
 from gluetool_modules_framework.libs.test_schedule import TestScheduleResult
 from gluetool_modules_framework.testing.test_schedule_tmt import gather_plan_results, TestScheduleEntry
 
@@ -23,11 +26,27 @@ def _load_assets(name):
     )
 
 
+def _set_run_outputs(monkeypatch, *outputs):
+    '''Monkey-patch gluetools.utils.Command.run to return given output'''
+    returns = map(lambda o: MagicMock(exit_code=0, stdout=o, stderr=''), outputs)
+    monkeypatch.setattr(gluetool.utils.Command, 'run', MagicMock(side_effect=returns))
+
+
 @pytest.fixture(name='module')
 def fixture_module():
     module = create_module(gluetool_modules_framework.testing.test_schedule_tmt.TestScheduleTMT)[1]
 
     return module
+
+
+@pytest.fixture(name='guest')
+def fixture_guest():
+    guest = MagicMock()
+    guest.name = 'guest0'
+    guest.hostname = 'guest0'
+    guest.key = 'mockkey0'
+    guest.execute = MagicMock(return_value=MagicMock(stdout='', stderr=''))
+    return guest
 
 
 def test_sanity(module):
@@ -74,3 +93,67 @@ def test_gather_results(module, asset, monkeypatch):
 
     assert outcome == getattr(TestScheduleResult, expected_results['outcome'])
     _assert_results(results, expected_results['results'])
+
+
+def test_serialize_test_schedule_entry_results(module, guest, monkeypatch):
+    # this doesn't appear anywhere in results.xml, but _run_plan() needs it
+    module_dist_git = create_module(DistGit)[1]
+    module_dist_git._repository = DistGitRepository(
+        module_dist_git, 'some-package',
+        clone_url='http://example.com/git/myproject', ref='myfix'
+    )
+    module.glue.add_shared('dist_git_repository', module_dist_git)
+
+    test_env = TestingEnvironment('x86_64', 'rhel-9')
+    schedule_entry = TestScheduleEntry(
+        gluetool.log.Logging().get_logger(),
+        test_env,
+        '/passed',
+        'some-repo-dir'
+    )
+    schedule_entry.guest = guest
+    schedule_entry.testing_environment = test_env
+
+    # gather_plan_results() is called in _run_plan() right after calling tmt; we need to inject
+    # writing results.yaml in between, which we can't do with a mock
+    orig_gather_plan_results = gluetool_modules_framework.testing.test_schedule_tmt.gather_plan_results
+
+    def inject_gather_plan_results(schedule_entry, work_dir):
+        shutil.copytree(os.path.join(ASSETS_DIR, 'passed'), os.path.join(work_dir, 'passed'))
+        return orig_gather_plan_results(schedule_entry, work_dir)
+
+    # run tmt with the mock plan
+    with monkeypatch.context() as m:
+        _set_run_outputs(m, 'dummy test done')
+        try:
+            gluetool_modules_framework.testing.test_schedule_tmt.gather_plan_results = inject_gather_plan_results
+            module.run_test_schedule_entry(schedule_entry)
+        finally:
+            gluetool_modules_framework.testing.test_schedule_tmt.gather_plan_results = orig_gather_plan_results
+
+    # generate results.xml
+    test_suite = gluetool.utils.new_xml_element('testsuite')
+    module.shared('serialize_test_schedule_entry_results', schedule_entry, test_suite)
+
+    assert test_suite['tests'] == 2
+    testcase_docs, testcase_dry = test_suite.contents
+    assert testcase_docs.name == 'testcase'
+    assert testcase_docs['name'] == '/tests/core/docs'
+    assert testcase_docs['result'] == 'passed'
+    # expecting log_dir and testout.log, in exactly that order; current code ignores journal.txt
+    assert len(testcase_docs.logs) == 2
+    assert testcase_docs.logs.contents[0].name == 'log'
+    assert testcase_docs.logs.contents[0]['name'] == 'log_dir'
+    assert testcase_docs.logs.contents[0]['href'].endswith('/passed/execute/logs/tests/core/docs')
+    assert testcase_docs.logs.contents[1]['name'] == 'testout.log'
+    assert testcase_docs.logs.contents[1]['href'].endswith('/passed/execute/logs/tests/core/docs/out.log')
+
+    assert testcase_dry['name'] == '/tests/core/dry'
+    assert testcase_dry['result'] == 'passed'
+    assert len(testcase_dry.logs) == 2
+    assert testcase_dry.logs.contents[0]['name'] == 'log_dir'
+    assert testcase_dry.logs.contents[0]['href'].endswith('/passed/execute/logs/tests/core/dry')
+    assert testcase_dry.logs.contents[1]['name'] == 'testout.log'
+    assert testcase_dry.logs.contents[1]['href'].endswith('/passed/execute/logs/tests/core/dry/out.log')
+
+    shutil.rmtree(schedule_entry.work_dirpath)

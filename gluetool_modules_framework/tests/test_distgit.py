cc4c06c00a3b1584b29b85eaecf2033c3fa10a7b
Guy Inger <ginger@redhat.com>
2022-11-27 15:23:47 +0200

    port distgit module from testing-farm branch

--- gluetool_modules_framework/tests/test_distgit.py
+++ gluetool_modules_framework/tests/test_distgit.py
@@ -15,7 +15,7 @@ from gluetool.log import Logging
 
 import gluetool_modules_framework.infrastructure.distgit
 from gluetool_modules_framework.infrastructure.distgit import DistGitRepository
-from . import assert_shared, create_module, patch_shared, testing_asset
+from . import create_module, patch_shared, testing_asset
 
 Response = collections.namedtuple('Response', ['status_code', 'content', 'text'])
 
@@ -27,7 +27,10 @@ def fixture_module():
 
 @pytest.fixture(name='dummy_repository')
 def fixture_dummy_repository(module):
-    return DistGitRepository(module, 'some-package', clone_url='some-clone-url', web_url='some-web-url', branch='some-branch')
+    return DistGitRepository(
+        module, 'some-package',
+        clone_url='some-clone-url', web_url='some-web-url', branch='some-branch'
+    )
 
 
 @pytest.fixture(name='dummy_repository_path')
@@ -44,6 +47,15 @@ def fixture_dummy_repository_path(module):
     }),
     ('selinux-policy', {
         '1782925', '1782925', '1779098', '1791557', '1790795', '1787298', '1778126', '1777761', '1777042'
+    })
+] if six.PY2 else [
+    ('systemd', {
+        bytes('1777110', 'utf-8'), bytes('1702565', 'utf-8')
+    }),
+    ('selinux-policy', {
+        bytes('1782925', 'utf-8'), bytes('1782925', 'utf-8'), bytes('1779098', 'utf-8'), bytes('1791557', 'utf-8'),
+        bytes('1790795', 'utf-8'), bytes('1787298', 'utf-8'), bytes('1778126', 'utf-8'), bytes('1777761', 'utf-8'),
+        bytes('1777042', 'utf-8')
     })
 ])
 def fixture_git_log(request, module, monkeypatch, dummy_repository):
@@ -92,10 +104,6 @@ def test_sanity_missing_required_options(module, method):
     with pytest.raises(gluetool.utils.IncompatibleOptionsError,
                        match="missing required options for method '{}'".format(method)):
         module.sanity()
-
-
-def test_missing_primary_task(module):
-    assert_shared('primary_task', module.execute)
 
 
 def test_artifact(monkeypatch, module):

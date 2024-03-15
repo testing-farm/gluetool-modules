4abda2b5e2e824f18fcbc7222d295563dd57a364
Miroslav Vadkerti <mvadkert@redhat.com>
2024-03-15 22:22:25 +0100

    libs/xunit: redact long output from printable representation
    In the `xunit` the system_out can be very long.
    Lately this caused issues where the logs exploaded, as the results
    are now part of the evaluation context. When they were written
    out in verbose log, this representation leaked the whole output
    there during each evaluation.
    
    This should redact the possible fields with huge content from the
    printable representation.
    
    Related to TFT-2495
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>
    
    
    (cherry picked from commit 65937d5bb73661593f305ae9f93086459618d477)

---
+++ gluetool_modules_framework/tests/test_libs_results.py
@@ -0,0 +1,24 @@
+# Copyright Contributors to the Testing Farm project.
+# SPDX-License-Identifier: Apache-2.0
+
+import textwrap
+
+import gluetool_modules_framework.libs.results.xunit as xunit
+
+
+def test_xunit_repr():
+    """ Test that class printable representation, large fields should be redacted """
+    testcase = xunit.XUnitTestCase(
+        name='testcase',
+        system_out=['output'],
+        classname='class-name',
+        failure=xunit.XUnitFailure(
+            type='fail',
+            message='message'
+        ),
+        time=1
+    )
+
+    assert str(testcase) == textwrap.dedent("""
+        XUnitTestCase(name='testcase', classname='class-name', failure=XUnitFailure(type='fail'), time=1)
+    """).strip()

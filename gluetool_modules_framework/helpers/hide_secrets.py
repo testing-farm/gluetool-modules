9af78b0d94fc1ebace1f39294d6e268d86e1a2f5
Evgeny Fedin <efedin@redhat.com>
2024-08-22 14:28:11 +0200

    hide-secrets: add retries to commands, make sync command pass not mandatory

--- gluetool_modules_framework/helpers/hide_secrets.py
+++ gluetool_modules_framework/helpers/hide_secrets.py
@@ -5,7 +5,12 @@ import tempfile
 
 import gluetool
 
+from gluetool.result import Result
+
 from typing import Any, Optional, List, Set, Union  # noqa
+
+DEFAULT_RETRY_TIMEOUT = 30
+DEFAULT_RETRY_TICK = 10
 
 
 class HideSecrets(gluetool.Module):
@@ -19,7 +24,19 @@ class HideSecrets(gluetool.Module):
         'search-path': {
             'help': 'Path used to search for files (default: %(default)s)',
             'default': '.'
-        }
+        },
+        'retry-tick': {
+            'help': 'Timeout between retries for failed operations. (default: %(default)s)',
+            'metavar': 'RETRY_TICK',
+            'type': int,
+            'default': DEFAULT_RETRY_TICK,
+        },
+        'retry-timeout': {
+            'help': 'Timeout for retries in seconds. (default: %(default)s)',
+            'metavar': 'RETRY_TIMEOUT',
+            'type': int,
+            'default': DEFAULT_RETRY_TIMEOUT,
+        },
     }
     shared_functions = ['add_secrets', 'hide_secrets']
 
@@ -73,15 +90,47 @@ class HideSecrets(gluetool.Module):
             temp.flush()
             command = "find '{}' -type f | xargs -i##### sed -i -f '{}' '#####'".format(search_path, temp.name)
 
-            output = gluetool.utils.Command([command]).run(shell=True)
+            def _run_sed() -> Result[bool, bool]:
+                try:
+                    output = gluetool.utils.Command([command], logger=self.logger).run(shell=True)
+                except gluetool.GlueCommandError as exc:
+                    self.warn('sync command "{}" failed, retrying: {}'.format(" ".join(cmd), exc))
+                    return Result.Error(False)
 
-        output.log(self.logger)
+                output.log(self.logger)
+                if output.exit_code != 0:
+                    raise gluetool.GlueError('Failed to hide secrets, secrets could be leaked!')
 
-        if output.exit_code != 0:
-            gluetool.GlueError('Failed to hide secrets, secrets could be leaked!')
+                return Result.Ok(True)
+
+            gluetool.utils.wait(
+                "Running '{}'".format(command),
+                _run_sed,
+                timeout=self.option('retry-timeout'),
+                tick=self.option('retry-tick')
+            )
 
         # Be paranoic that modified files were written to the disk
-        gluetool.utils.Command(['sync', search_path]).run()
+        cmd = ['sync', search_path]
+
+        def _run_sync() -> Result[bool, bool]:
+            try:
+                gluetool.utils.Command(cmd, logger=self.logger).run()
+            except gluetool.GlueCommandError as exc:
+                self.warn('sync command "{}" failed, retrying: {}'.format(" ".join(cmd), exc))
+                return Result.Error(False)
+            return Result.Ok(True)
+
+        try:
+            gluetool.utils.wait(
+                "sync of '{}'".format(search_path),
+                _run_sync,
+                timeout=self.option('retry-timeout'),
+                tick=self.option('retry-tick')
+            )
+
+        except gluetool.GlueError:
+            self.warn('Failed to sync modified files to disk.')
 
     def destroy(self, failure: Optional[Any] = None) -> None:
         self.hide_secrets()

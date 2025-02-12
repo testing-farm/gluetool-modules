c94e945a36ceb5d379c22f4351f25c8f48caaf48
Evgeny Fedin <efedin@redhat.com>
2025-02-12 14:36:04 +0100

    hide-secrets: escape new line with thee backslashes

--- gluetool_modules_framework/tests/test_hide_secrets.py
+++ gluetool_modules_framework/tests/test_hide_secrets.py
@@ -37,6 +37,13 @@ qui officia deserunt mollit anim id est laborum.
 
 LONG_SECRET = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat. Duis auteirure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa  ui officia deserunt mollit anim id est laborum."
 
+SSH_KEY_SECRET = """-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAACFwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAgEAwbGBzDKGiZ/2VI6KjPcaWaF4mmPIVPDe+tJs4KiThR3HDskX/U0/
+ACDlW+fVSZAPjFuoBxZ4GoAcOcYRAhcT0OoZnZwbij27Tdot9KGTuxWlTqFh4wIvDyMe2E
+3b8/9lS0bTI5AAAAE2V4YW1wbGVAZXhhbXBsZS5jb20BAgMEBQYH
+-----END OPENSSH PRIVATE KEY-----"""
+
 
 @pytest.mark.parametrize('testing_farm_request', [
     (MagicMock(environments_requested=[TestingEnvironment(secrets={'secret': 'foo'})])),
@@ -131,6 +138,35 @@ def test_hide_secrets_yaml_dump_long_string(monkeypatch, module):
 
         with open(os.path.join(tmpdir, 'testfile.yml'), 'r') as f:
             assert f.read() == 'test: foo\nsecret: hidden\n'
+
+
+# In tmt verbose log multiline secrets have indentation and we need to make sure
+# that we hide them correctly, e.g.
+# SSH_KEY_SECRET:
+#    -----BEGIN OPENSSH PRIVATE KEY-----
+#    b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAACFwAAAAdzc2gtcn
+#    NhAAAAAwEAAQAAAgEAwbGBzDKGiZ/2VI6KjPcaWaF4mmPIVPDe+tJs4KiThR3HDskX/U0/
+#    ACDlW+fVSZAPjFuoBxZ4GoAcOcYRAhcT0OoZnZwbij27Tdot9KGTuxWlTqFh4wIvDyMe2E
+#    3b8/9lS0bTI5AAAAE2V4YW1wbGVAZXhhbXBsZS5jb20BAgMEBQYH
+#    -----END OPENSSH PRIVATE KEY-----
+def test_hide_secrets_multiline_indentation(monkeypatch, module):
+    with tempfile.TemporaryDirectory(prefix='hide_secrets', dir=ASSETS_DIR) as tmpdir:
+        module._config['search-path'] = tmpdir
+
+        with open(os.path.join(tmpdir, 'testfile.txt'), 'w') as f:
+            f.write("SSH_KEY_SECRET: {}".format(SSH_KEY_SECRET.replace('\n', '\n    ')))
+            f.flush()
+
+        testing_farm_request = MagicMock(
+            environments_requested=[TestingEnvironment(secrets={'SSH_KEY_SECRET': SSH_KEY_SECRET})])
+
+        module._config['search-path'] = tmpdir
+
+        module.add_secrets([SSH_KEY_SECRET])
+        module.destroy()
+
+        with open(os.path.join(tmpdir, 'testfile.txt'), 'r') as f:
+            assert f.read() == 'SSH_KEY_SECRET: hidden'
 
 
 def test_hide_secrets_multiple(monkeypatch, module):

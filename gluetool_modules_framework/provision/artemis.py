74d31ce0ebbb908e78a2bc95c86d9028dadbac19
Evgeny Fedin <efedin@redhat.com>
2025-03-26 14:45:00 +0100

    artemis: add user-data-vars-template-file to artemis_api_options

--- gluetool_modules_framework/provision/artemis.py
+++ gluetool_modules_framework/provision/artemis.py
@@ -1165,6 +1165,7 @@ class ArtemisProvisioner(gluetool.Module):
             'ready-timeout': self.option('ready-timeout'),
             'ready-tick': self.option('ready-tick'),
             'api-call-timeout': self.option('api-call-timeout'),
+            'user-data-vars-template-file': self.option('user-data-vars-template-file')
         }
 
     @property

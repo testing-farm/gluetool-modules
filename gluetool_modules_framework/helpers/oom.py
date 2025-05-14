ed66888e2f1f4b242c88707bf86e51c28238f45d
Miroslav Vadkerti <mvadkert@redhat.com>
2025-05-14 03:00:10 +0200

    Add module for monitoring out-of-memory for pipeline
    The module will provide a simple way how to monitor memory
    consumption from inside the pipeline and:
    
    1. send and alert to Sentry if the memory consumption is over
       reservation memory (once)
    
    2. terminate pipeline if the memory consumption is over
       the limit
    
    As the check is very cheap, it is done each `5 seconds`,
    by default via `RepeatTimer` we use in other places.
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

---
+++ gluetool_modules_framework/helpers/oom.py
@@ -0,0 +1,173 @@
+# Copyright Contributors to the Testing Farm project.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+from contextlib import nullcontext
+
+import psutil
+from gluetool import Failure, Module
+from gluetool.log import log_dict
+from gluetool_modules_framework.libs.threading import RepeatTimer
+
+# Type annotations
+# pylint: disable=unused-import,wrong-import-order
+from typing import Any, Optional  # noqa
+
+# Default tick in seconds between checks for OOM
+DEFAULT_OOM_CHECK_TICK = 5
+
+# File containing the current memory consumption
+DEFAULT_CGROUPS_MEMORY_CURRENT_PATH = "/sys/fs/cgroup/memory.current"
+
+
+class OutOfMemory(Module):
+    """
+    Handle out-of-memory events for the pipeline.
+
+    Checks memory consumption via cgroups v2 memory controller.
+
+    If the ``reservation`` memory limit is reached, it sends a warning to Sentry.
+
+    If the memory ``limit`` is reached, it terminates the terminates the pipeline.
+
+    The module will work only if run in a container engine using cgroups v2, where the
+    memory consumption can be detected by reading ``/sys/fs/cgroup/memory.current`` file.
+    If the file does not exist, the module just emits a warning and does nothing.
+    """
+
+    name = "oom"
+    description = "Handle out-of-memory events for the pipeline."
+
+    options = [
+        (
+            "Memory",
+            {
+                "reservation": {
+                    "help": "The expected memory reservation in MiB. Emits a single warning to Sentry once breached.",
+                    "type": int,
+                    "metavar": "MiB",
+                },
+                "limit": {
+                    "help": "The maximum memory consumption in MiB. If breached the pipeline is cancelled.",
+                    "type": int,
+                    "metavar": "MiB",
+                },
+                "monitoring-path": {
+                    "help": "The file used to read current memory consumption in bytes. (default: %(default)s).",
+                    "default": DEFAULT_CGROUPS_MEMORY_CURRENT_PATH,
+                },
+            },
+        ),
+        (
+            "General",
+            {
+                "enabled": {
+                    "help": "Enable out-of-memory checking. (default: %(default)s).",
+                    "action": "store_true",
+                    "default": "yes"
+                },
+                "tick": {
+                    "help": """
+                        Number of seconds to wait between checking memory consumption again. (default: %(default)s)
+                    """,
+                    "type": int,
+                    "default": DEFAULT_OOM_CHECK_TICK,
+                },
+                "verbose": {
+                    "help": "Enable verbose logging for memory checking.",
+                    "action": "store_true",
+                },
+            },
+        ),
+    ]
+
+    required_options = ('reservation', 'limit')
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super(OutOfMemory, self).__init__(*args, **kwargs)
+        self._oom_timer: Optional[RepeatTimer] = None
+        self._reservation_reached: bool = False
+
+    def terminate_pipeline(self) -> None:
+        """
+        Terminate the pipeline by terminating the current process.
+        """
+        self.error(
+            'Terminating pipeline because worker is out of memory, more than {} MiB consumed.'.format(
+                self.option('limit')
+            ),
+            sentry=True
+        )
+
+        # stop the repeat timer, it will not be needed anymore
+        self.debug('Stopping pipeline oom check')
+        if self._oom_timer:
+            self._oom_timer.cancel()
+            self._oom_timer = None
+
+        # cancel the pipeline
+        psutil.Process().terminate()
+
+    def handle_oom(self) -> None:
+        """
+        Handle out-of-memory events.
+        """
+
+        reservation = self.option('reservation')
+        limit = self.option('limit')
+
+        with open(self.option('monitoring-path')) as f:
+            memory_consumed = int(f.read()) / 1024**2
+
+        if self.option('verbose'):
+            log_dict(self.debug, 'out-of-memory check', {
+                'memory': '{:.2f} MiB'.format(memory_consumed),
+                'reserved': '{} MiB'.format(reservation),
+                'limit': '{} MiB'.format(limit)
+            })
+
+        if not self._reservation_reached and memory_consumed > reservation:
+            self.warn(
+                "Reservation memory '{}' reached".format(reservation),
+                sentry=True
+            )
+            self._reservation_reached = True
+
+        if memory_consumed > limit:
+            with self.shared('pipeline_cancellation_lock') or nullcontext():
+                self.terminate_pipeline()
+
+    def destroy(self, failure: Optional[Failure] = None) -> None:
+        # stop the repeat timer, it will not be needed anymore
+        if self._oom_timer:
+            self.info('Stopping out-of-memory monitoring')
+            self._oom_timer.cancel()
+            self._oom_timer = None
+
+    def execute(self) -> None:
+        if not self.option('enabled'):
+            self.info('Out-of-memory monitoring is disabled')
+            return
+
+        monitoring_path = self.option('monitoring-path')
+
+        if not os.path.exists(monitoring_path):
+            self.warn(
+                "Out-of-memory monitoring is unavailable, '{}' not available".format(monitoring_path)
+            )
+            return
+
+        log_dict(
+            self.info,
+            'Starting out-of-memory monitoring, check every {} seconds'.format(self.option('tick')),
+            {
+                'reserved': '{} MiB'.format(self.option('reservation')),
+                'limit': '{} MiB'.format(self.option('limit')),
+                'monitoring': monitoring_path
+            }
+        )
+
+        self._oom_timer = RepeatTimer(
+            self.option('tick'), self.handle_oom
+        )
+        self._oom_timer.start()

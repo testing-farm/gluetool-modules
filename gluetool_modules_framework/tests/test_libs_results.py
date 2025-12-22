3cc543f2f6698046c12647b58bed4eeafb5e693b
Miroslav Vadkerti <mvadkert@redhat.com>
2025-12-22 15:48:36 +0100

    results: expose errors under <error> element for testcases
    We used `<failure>` element for errors, which seems wrong and causing
    confusion. Seems errors are normally recognizable in xunit and one
    can use the `<error>` element.
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- gluetool_modules_framework/tests/test_libs_results.py
+++ gluetool_modules_framework/tests/test_libs_results.py
@@ -20,5 +20,5 @@ def test_xunit_repr():
     )
 
     assert str(testcase) == textwrap.dedent("""
-        XUnitTestCase(name='testcase', classname='class-name', failure=XUnitFailure(type='fail'), time=1, start_time=None, end_time=None)
+        XUnitTestCase(name='testcase', classname='class-name', failure=XUnitFailure(type='fail'), error=None, time=1, start_time=None, end_time=None)
     """).strip()
